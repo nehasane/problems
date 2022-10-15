f2f25139f1f6f14344b709b0d5101d015df057e9
Neha Sane <97963115+nehasane@users.noreply.github.com>
2022-10-15 19:57:42 +0530

    Update ioi00p1.cpp

--- dmoj/ioi00p1.cpp
+++ dmoj/ioi00p1.cpp
@@ -1,3 +1,50 @@
+/*
+
+without the memory constraints, the problem is quite simple to solve (v1):
+    loop over each character S[i] from 0 TO n-1 and treat it as either the
+    middle of the palindrome or the left of the two middle elements of the
+    palindrome. then, have a left and right pointer: if the two characters
+    pointed by the pointers are the same, then there's no need to add an 
+    extra character. otherwuse, we need to add either an extra S[l] character,
+    or an extra S[r] character.
+    
+    basically,
+        dp[l][r] = minimum number of characters that need to be added if we
+        consider the string S[l...r]
+    implementing this using tabular or recursive dp will reult in MLE.
+    memory = (5000 * 5000 * 4 * 2) / (1024 * 1024) ~~ 96 MB
+    
+    instead, think of this as an LCS problem.
+    have two strings: S and Sr, where S is the original string and Sr is
+    the reverse of S. Let's find the length of the LCS of the two strings.
+    the LCS will tell us number of characters that don't need to be added,
+    because if a character is common between a string and its reverse, then
+    we know that that character is already paired up, and that it will read 
+    the same left to right.
+    
+    a typical implementation will also run in MLE, but there's a neat trick for
+    that (v2)
+        the dp formula for calulating length of LCS where i is the index of the
+        ith character of the first string, and j is the index of the jth 
+        character of the second string.
+        if (S[i] == Sr[j])
+            dp[i][j] = dp[i-1][j-1] + 1;
+        else
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        notice that in any case we are only accessing the previous row (or column)
+        so we use only two rows in our dp table, where one row contains the dp
+        values for the previous i, while the second row contains the dp values for
+        the current i. we can alternate between the two rows.
+        
+        so the dp table will look like this: dp[2][MAX_COLUMN]
+        and the dp formula like this
+            if (S[i] == Sr[j])
+                dp[curr_row][j] = dp[prev_row][j] + 1;
+            else
+                dp[curr_row][j] = dp[prev_row][j], dp[i][j-1];
+        
+
+*/
 // v1
 #include <bits/stdc++.h>
 using namespace std;
