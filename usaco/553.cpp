2eca08233aed1a489812f73db54be2388104ea59
Neha Sane <97963115+nehasane@users.noreply.github.com>
2023-08-10 16:09:50 +0530

    Update 553.cpp

--- usaco/553.cpp
+++ usaco/553.cpp
@@ -67,3 +67,54 @@ int main() {
 
 
 }
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+const int mod = 1e9 + 7;
+int n;
+vector <string> grid;
+vector <vector <unordered_map <int, int>>> dp;
+int solve(int r1, int r2, int a) {
+    int c1 = (n-1) - a - r1;
+    int c2 = n - ((n-1) - a - (n-1-r2)) - 1;
+    if (r1 < 0 || r2 < 0 || r1 >= n || r2 >= n || a < 0)
+        return 0;
+    if (grid[r1][c1] != grid[r2][c2])
+        return 0;
+    if (r1 == r2 && c1 == c2)
+        return 1;
+    if (dp[r1][r2].find(a) != dp[r1][r2].end())
+        return dp[r1][r2][a];
+    int ans = solve(r1, r2, a-1);
+    ans = (ans + solve(r1+1, r2, a-1)) % mod;
+    ans = (ans + solve(r1+1, r2-1, a-1)) % mod;
+    ans = (ans + solve(r1, r2-1, a-1)) % mod;
+    dp[r1][r2][a] = ans;
+    return ans;
+
+}
+int main() {
+    freopen("palpath.in", "r", stdin);
+    freopen("palpath.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    grid.resize(n);
+    dp.resize(n);
+    for (int i = 0; i < n; i++) {
+        dp[i].resize(n);
+    }
+    for (int i = 0; i < n; i++)
+        cin >> grid[i];
+    int ans = solve(0, n-1, n-1);
+    cout << ans << '\n';
+
+
+
+
+}
